0e1349c61509ad957b3ce120561ce5d8ccf02af9
SaitejareddyTileti-0293 <saitejareddytileti@gmail.com>
2021-03-24 09:43:15 +0530

    feat: load messages

--- public/scripts/loadMessages.js
+++ public/scripts/loadMessages.js
@@ -10,7 +10,32 @@
 let messageListElement = document.getElementById("messages");
 
 //--------------------functions------------------------
-function loadMessages() {}
+function loadMessages() {
+  let query = firebase
+    .firestore()
+    .collection("messages")
+    .orderBy("timestamp", "desc")
+    .limit(12);
+
+  query.onSnapshot(function (snapshot) {
+    console.log("snapshot", snapshot);
+    snapshot.docChanges().forEach(function (change) {
+      if (change.type === "removed") {
+        deleteMessage(change.doc.id);
+      } else {
+        let message = change.doc.data();
+        displayMessage(
+          change.doc.id,
+          message.timestamp,
+          message.name,
+          message.text,
+          message.profilePicUrl,
+          message.imageUrl
+        );
+      }
+    });
+  });
+}
 
 function deleteMessage(id) {
   let div = document.getElementById(id);
